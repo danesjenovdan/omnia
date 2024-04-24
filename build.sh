e31d8a9a2684eeb102a52aeacf75a7b9590bc8ee
LepkoQQ <lepko.san@gmail.com>
2024-04-24 10:28:22 +0200

    change url and build name for omnia-evropske-2024

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-sudo docker build -f Dockerfile -t omnia-predsednik-2022:latest .
-sudo docker tag omnia-predsednik-2022:latest rg.fr-par.scw.cloud/djnd/omnia-predsednik-2022:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/omnia-predsednik-2022:latest
+sudo docker build -f Dockerfile -t omnia-evropske-2024:latest .
+sudo docker tag omnia-evropske-2024:latest rg.fr-par.scw.cloud/djnd/omnia-evropske-2024:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/omnia-evropske-2024:latest
