fbf28913042c9c6b96dae26523ef54724cd1cc9b
LepkoQQ <lepko.san@gmail.com>
2024-10-15 13:25:13 +0200

    update for jek2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-sudo docker build -f Dockerfile -t omnia-evropske-2024:latest .
-sudo docker tag omnia-evropske-2024:latest rg.fr-par.scw.cloud/djnd/omnia-evropske-2024:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/omnia-evropske-2024:latest
+sudo docker build -f Dockerfile -t omnia-referendum-jek2:latest .
+sudo docker tag omnia-referendum-jek2:latest rg.fr-par.scw.cloud/djnd/omnia-referendum-jek2:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/omnia-referendum-jek2:latest
