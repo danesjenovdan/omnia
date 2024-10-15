2a0c323133d0672e45edfb0faa10ee1e806620a7
LepkoQQ <lepko.san@gmail.com>
2024-10-15 13:36:33 +0200

    update for jek2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-sudo docker build -f Dockerfile -t predcasno-glasovanje-evropske-2024:latest .
-sudo docker tag predcasno-glasovanje-evropske-2024:latest rg.fr-par.scw.cloud/djnd/predcasno-glasovanje-evropske-2024:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/predcasno-glasovanje-evropske-2024:latest
+sudo docker build -f Dockerfile -t predcasno-glasovanje-referendum-jek2:latest .
+sudo docker tag predcasno-glasovanje-referendum-jek2:latest rg.fr-par.scw.cloud/djnd/predcasno-glasovanje-referendum-jek2:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/predcasno-glasovanje-referendum-jek2:latest
