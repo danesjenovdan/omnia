305e9d0062f36e1694b52694b929a0827e492a64
LepkoQQ <lepko.san@gmail.com>
2025-11-04 12:43:22 +0100

    style fixes, share changes, mobile styles

--- scripts/main.js
+++ scripts/main.js
@@ -772,20 +772,28 @@ var map, ajaxRequest, plotlist, plotlayers = [],
     ];
 
 $(document).ready(function () {
-    $(".thetime").text(timeLeft()), initmap("themap"), $(".zavesa .button").on("click", function () {
-        $(".zavesa").animate({
-            top: -1e3
-        }, 600), $(".header .circle").removeClass("hidden")
-    }), $("#themap").on("click", ".namera", function () {
-        // window.open("https://e-uprava.gov.si/podrocja/vloge/vloga.html?id=5626 ", "_blank");
-        // $(".stepone").addClass("hidden"), $(".steptwo").removeClass("hidden"), $(".header .circle").addClass("hidden"), $(".zavesa").animate({
-        //     top: 75
-        // }, 600)
-    }), $("body").on("click", ".tw", function () {
-        var a = "https://twitter.com/intent/tweet?text=" + encodeURIComponent("Boš na dan referenduma o ZPPKŽ na izletu po Sloveniji, pa želiš vseeno glasovati? 👀\n\nBrez skrbi, to lahko storiš na voliščih OMNIA!\n\nNajbližjega poišči na zemljevidu na spodnji povezavi, še prej pa svojo namero sporoči volilni komisiji! ✊\n\n" + document.location.href);
-        return window.open(a, "_blank"), !1
-    }), $("body").on("click", ".email", function () {
-        var a = "mailto:?subject=Tukaj lahko glasuješ, če boš na dan referenduma o ZPPKŽ na izletu po Sloveniji.&body=Živjo!%0D%0A%0D%0AVeš, da lahko na volitvah svoj glas oddaš tudi, če na dan referenduma o ZPPKŽ na izletu izven kraja stalnega prebivališča?%0D%0A%0D%0ATo lahko storiš na enem od volišč OMNIA, najbližjega pa lahko najdeš s pomočjo zemljevida na tej povezavi: " + document.location.href + "%0D%0A%0D%0AA ne pozabi, da moraš svojo namero za glasovanje na volišču OMNIA sporočiti volilni komisiji v svojem okraju najkasneje do 19. novembra.%0D%0A%0D%0APovezavo deli tudi z vsemi prijateljicami in prijatelji!%0D%0A%0D%0ALepo bodi.";
-        return window.open(a, "_blank"), !1
-    })
+    $(".thetime").text(timeLeft());
+
+    initmap("themap");
+
+    $(".zavesa .close-button").on("click", function () {
+        $(".zavesa").animate({ top: -1000 }, 600)
+    });
+
+    $(".zavesa .share input").val(window.location.href);
+    $(".zavesa .share input").on("focus", function () {
+        $(this).select();
+    });
+
+    $(".zavesa .share .copy-button").on("click", function () {
+        var copyText = $(".zavesa .share input");
+        copyText.select();
+        document.execCommand("copy");
+        var oldHtml = $(this).html();
+        $(this).html("Kopirano!");
+        var button = $(this);
+        setTimeout(function () {
+            button.html(oldHtml);
+        }, 2000);
+    });
 });
