407da443edbd79fe0e5f3437f8c6531c7e4259f9
LepkoQQ <lepko.san@gmail.com>
2025-11-04 15:27:31 +0100

    fix popup width and dont break on smileys

--- scripts/main.js
+++ scripts/main.js
@@ -22,16 +22,16 @@ function initmap(container) {
 
         var markup = '<div class="markercontainer"><div><b>' + b.ime + "</b></div><div>" + b.naslov + ", " + b["pošta"] + "</div><div><br>Volilni okraj: <strong>" + b.okraj + '</strong></div>';
         if ("i" === b.dostopnoInvalidom) {
-            markup += '<div><br><b>Volišče je dostopno invalidom. :)</b></div>'
+            markup += '<div><br><b>Volišče je dostopno invalidom. <nobr>:)</nobr></b></div>'
         } else {
-            markup += '<div><br><b>Ne vemo, ali je volišče dostopno invalidom. :(</b></div>'
+            markup += '<div><br><b>Ne vemo, ali je volišče dostopno invalidom. <nobr>:(</nobr></b></div>'
         }
 
         markup += '<div><br><b>Oddaj vlogo <a href="https://e-uprava.gov.si/si/podrocja/vloge/vloga.html?id=8765" target="_blank" rel="noopener noreferrer">z e-identiteto</a> ali <a href="https://e-uprava.gov.si/si/podrocja/vloge/vloga.html?id=8766" target="_blank" rel="noopener noreferrer">brez</a>.</b></div>'
 
         markup += '</div>'
 
-        c.bindPopup(markup)
+        c.bindPopup(markup, { minWidth: 300, maxWidth: 350 })
     })
 }
 
